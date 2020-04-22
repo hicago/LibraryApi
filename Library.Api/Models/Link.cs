c841424a5fa273944f075d8953f1cfe126a8c8a2
hicago <hicago@live.cn>
2020-04-22 20:52:16 +0800

    7.4.1 HATEOAS

---
+++ Library.Api/Models/Link.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Api.Models
+{
+    public class Link
+    {
+        public Link(string method, string rel, string href)
+        {
+            Method = method;
+            Relation = rel;
+            Href = href;
+        }
+
+        public string Href { get; }
+        public string Method { get; }
+
+        [JsonProperty("rel")]
+        public string Relation { get; }
+    }
+}
---
+++ Library.Api/Models/Resource.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Library.Api.Models
+{
+    public class Resource
+    {
+        [JsonProperty("_link", Order= 100)]
+        public List<Link> Links { get; } = new List<Link>();
+    }
+
+    public class ResourceCollection<T> : Resource
+        where T : Resource
+    {
+        public List<T> Items { get; }
+
+        public ResourceCollection(List<T> items)
+        {
+            Items = items;
+        }
+    }
+}
